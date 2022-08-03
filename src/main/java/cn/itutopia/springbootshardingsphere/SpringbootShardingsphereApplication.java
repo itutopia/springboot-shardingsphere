6815c405a2c1bf0d08010b87422d1355f4ece7cf
junchao.li <junchao.li@tongfudun.com>
2022-08-03 10:39:26 +0800

    初始化项目

---
+++ src/main/java/cn/itutopia/springbootshardingsphere/SpringbootShardingsphereApplication.java
@@ -0,0 +1,13 @@
+package cn.itutopia.springbootshardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShardingsphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShardingsphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/itutopia/springbootshardingsphere/SpringbootShardingsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itutopia.springbootshardingsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShardingsphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
